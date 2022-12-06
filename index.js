266d5fa13cca267f643b3cef06ca3252e1289331
umurzoq04 <zoyirovumurzoq31@gmail.com>
2022-12-06 16:38:53 +0500

    comment

---
+++ index.js
@@ -0,0 +1,16 @@
+var massa = Number(prompt("Assalom aleykum iltimos vazningizni kiriting"));
+var uzunlik = Number(prompt("Iltimos bo'yingizni balandligini kiriting (masalan: 1.83)"));
+var BMI = Number(massa/(uzunlik * uzunlik))
+
+
+ if (BMI < 18){
+    alert(`Sizda vazningiz bo'yicha muammo bor`)
+}
+else if (BMI >= 15 & BMI <=25){
+    alert(`juda yaxshi siz sog'lomsiz`);
+}
+else if (BMI > 25){
+    alert(`Sizda vazn bo'yicha muammo bor ozing!`);
+}
+
+
